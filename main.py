2bdff582cf5115ca5366a56908fca82031462817
ulises-gomez-dev <ulises.gomez.dev@proton.me>
2025-08-01 21:50:48 -0700

    added game loop using blessed

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from blessed import Terminal
+from ollama import ChatResponse, chat
+
+
+def render_logo(width, path="assets/ascii/logo.txt"):
+    with open(path, "r") as f:
+        for line in f:
+            print(line.strip("\n").center(width))
+
 
 def main():
     term = Terminal()
 
-    with term.cbreak(), term.hidden_cursor():
-        break
+    with term.fullscreen(), term.cbreak(), term.hidden_cursor():
+        # Clear the screen
+        print("\033[2J\033[3J\033[H")
+
+        render_logo(term.width)
+
+        loop = True
+        while loop:
+            
+            key = term.inkey()
+            
+            if key.lower() == "q":
+                loop = False
+
 
 if __name__ == "__main__":
     main()
